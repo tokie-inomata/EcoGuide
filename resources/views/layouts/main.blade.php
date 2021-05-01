e6a1ba6726490e83ffe4bb9559e1b6dfdd19d6b3
tokie-inomata <asahane06@gmail.com>
2021-05-01 19:57:44 +0900

    トップ画面

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+</head>
+<body>
+    <div id="header">
+        <img alt="ロゴ" href="/" class="titlelogo">
+        <nav>
+            <ul>
+                <ol><a href="/">ログイン</a></ol>
+                <ol><a href="/">新規登録</a></ol>
+            </ul>
+        </nav>
+    </div>
+
+    @yield('main')
+
+    <div id="footer">
+        <p class="copyright">Copyright ©︎ 2021 EcoGuide</p>
+        <a class="inquiry" href="/">お問い合わせ</a>
+    </div>
+</body>
+</html>
