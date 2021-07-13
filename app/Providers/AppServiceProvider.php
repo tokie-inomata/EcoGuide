16f2076ed28c80709ee4683fcb4f124dac99534c
tokie-inomata <asahane06@gmail.com>
2021-07-14 03:47:02 +0900

    SSL対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        $url->forceScheme('https');
+
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
