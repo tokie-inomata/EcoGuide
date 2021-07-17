34e6e6bf1bd00035dc9eb17409189ab9a3524fe6
tokie-inomata <asahane06@gmail.com>
2021-07-17 09:40:03 +0900

    パスワードリセットのメール内容変更

--- resources/views/emails/reset.blade.php
+++ resources/views/emails/reset.blade.php
@@ -1,5 +1,20 @@
-<p>
-    下記URLをクリックして、パスワードを再設定してください。<br>
-</p>
+<head>
+    <style>
+        h3 {
+            text-align: center;
+            font-size: 20pt;
+        }
+        p {
+            text-align: center;
+            line-height: 3.5;
+        }
+    </style>
+</head>
+<body>
+    <h3>パスワードのリセットが要求されました</h3>
+    <p>
+        24時間以内に下記URLをクリックして、パスワードを再設定してください。<br>
+    </p>
 
-{{ $actionUrl }}
+    <p>{{ $actionUrl }}</p>
+</body>
